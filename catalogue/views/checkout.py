7870f8d002df5663ceb300379f6be8f66d4d06b9
rider-mateo <rider.matt@studentbody.byu.edu>
2015-03-07 23:21:00 -0700

    make the checkout and receipt pages

--- catalogue/views/checkout.py
+++ catalogue/views/checkout.py
@@ -17,7 +17,39 @@ templater = get_renderer('catalogue')
 
 @view_function
 def process_request(request):
+
+    if not request.user.is_authenticated():
+        return redirect('/account/index/?next=%s' % request.path)
+
     params = {}
 
+    user = request.user
+
+    form = checkoutForm()
+
+    if request.method == 'POST':
+        form = checkoutForm(request.POST)
+        form.userid = user.id
+        if form.is_valid():
+            user.save();
+            return HttpResponseRedirect('/catalogue/checkout/${ user.id }')
+
     # add params
+    params['form'] = form
+    params['user'] = user
     return templater.render_to_response(request, 'checkout.html', params)
+
+class checkoutForm(forms.Form):
+    first_name = forms.CharField(required=True, max_length=100)
+    last_name = forms.CharField(required=True, max_length=100)
+    email = forms.EmailField(required=True, max_length=100)
+    billing_address = forms.CharField(max_length=100)
+    city = forms.CharField(max_length=30)
+    state = forms.CharField(max_length=2)
+    zip = forms.IntegerField()
+    country = forms.CharField(max_length=30)
+    phone = forms.CharField(max_length=12)
+    credit_card = forms.CharField(min_length=16)
+    csv = forms.CharField(max_length=3)
+
+
