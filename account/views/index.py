58dfe13d268c97c89ea2438a2e2f64aabc48e091
rider-mateo <rider.matt@studentbody.byu.edu>
2015-03-21 16:27:59 -0600

    the label tags on the checkout form got mixed up with the labels for login form so I deleted the labels from the checkout and add some css to the login form

--- account/views/index.py
+++ account/views/index.py
@@ -34,8 +34,10 @@ def process_request(request):
 
 
 class LoginForm(forms.Form):
-    username = forms.CharField(required=True, min_length=1, max_length=100, label="Username", widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'john.doe19'}))
-    password = forms.CharField(required=True, min_length=1, max_length=100, label="Password", widget=forms.PasswordInput(attrs={'class': 'form-control'}))
+    username = forms.CharField(required=True, min_length=1, max_length=100, label="Username", widget=forms.TextInput(
+        attrs={'class': 'form-control login-form', 'placeholder': 'john.doe19'}))
+    password = forms.CharField(required=True, min_length=1, max_length=100, label="Password", widget=forms.PasswordInput(
+        attrs={'class': 'form-control login-form'}))
 
     def clean(self):
         user = authenticate(username=self.cleaned_data.get('username'), password=self.cleaned_data.get('password'))
