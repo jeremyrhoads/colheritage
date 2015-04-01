9788b31a7cda31b048ec55a3906892716fb8bdee
rider-mateo <rider.matt@studentbody.byu.edu>
2015-04-01 17:46:34 -0600

    added the hard coded REST API call for the checkout process

--- catalogue/views/checkout.py
+++ catalogue/views/checkout.py
@@ -7,6 +7,7 @@ from django import forms
 from django_mako_plus.controller import view_function
 import homepage.models as hmod
 from django_mako_plus.controller.router import get_renderer
+import requests
 
 
 templater = get_renderer('catalogue')
@@ -31,7 +32,7 @@ def process_request(request):
         form = checkoutForm(request.POST)
         form.userid = user.id
         if form.is_valid():
-            user.save();
+            user.save()
             return HttpResponseRedirect('/catalogue/checkout/${ user.id }')
 
     # add params
@@ -53,3 +54,32 @@ class checkoutForm(forms.Form):
     csv = forms.CharField(max_length=3)
 
 
+@view_function
+def checkout(request):
+
+    API_URL = "http://dithers.cs.byu.edu/iscore/api/v1/charges"
+    API_KEY = "b6a57a6b718b756064393dd12588130d"  # KEY 1-12 2
+
+    r = requests.post(API_URL, data={
+        'name': 'Cosmo Limesandal',
+        'description': 'Charge for cosmo@is411.byu.edu',
+        'apiKey': API_KEY,
+        'currency': 'usd',
+        'amount': '5.99',
+        'type': 'Visa',
+        'number': '4732817300654',
+        'exp_month': '10',
+        'exp_year': '15',
+        'cvc': '411',
+    })
+
+    print(r.text)
+
+    resp = r.json()
+    if 'error' in resp:
+        print("ERROR: ", resp['error'])
+    else:
+        print(resp.keys())
+        print(resp['ID'])
+
+    return HttpResponseRedirect('/catalogue/receipt')
