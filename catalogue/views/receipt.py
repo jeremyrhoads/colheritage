6dcccff4d0eeb8ee4000e3f50bc1454086a626df
rider-mateo <rider.matt@studentbody.byu.edu>
2015-04-04 01:08:38 -0600

    added the skeleton for receipt emails and the base logic. Still need to add EMAIL_HOST and EMAIL_PORT to settings

--- catalogue/views/receipt.py
+++ catalogue/views/receipt.py
@@ -7,6 +7,7 @@ from django import forms
 from django_mako_plus.controller import view_function
 import homepage.models as hmod
 from django_mako_plus.controller.router import get_renderer
+from django.core.mail import send_mail
 
 
 templater = get_renderer('catalogue')
@@ -26,6 +27,12 @@ def email_receipt(request):
     params = {}
 
     # email logic goes here
+    email_subject = "Your CHF Receipt"
+    email_body = templater.render(request,'receipt_email.html', params)
+    from_email = 'admin@colheritagefoundation.com'
+    to_email = 'riderm17@gmail.com'
+
+    send_mail(email_subject, email_body, from_email, [to_email], html_message=email_body, fail_silently=False)
 
     print('>>>>>>>>>>>>>>>> testing')
 
